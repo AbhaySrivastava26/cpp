55178001a710fc970dca2f12523c67f938ce6685
AbhaySrivastava26 <abhaysrivastava2627@gmail.com>
2025-05-05 23:45:26 +0530

    lc

---
+++ LeetcodeOwn/771.cpp
@@ -0,0 +1,14 @@
+// class Solution {
+//     public:
+//         int numJewelsInStones(string jewels, string stones) {
+//     int count=0;
+//             for(int i=0;i<stones.size();i++){
+//                   for(auto ch:jewels){
+//                     if(ch==stones[i]){
+//                         count++;
+//                     }
+//                   }
+//         }
+//         return count;
+//         }
+//     };
